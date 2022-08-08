fea4695fb88f956cd2aa0a474575fa5c39dc413f
Zachary Haggerty <zhaggerty24@gmail.com>
2022-08-08 00:20:52 -0400

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Operator_Exercise
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            
+
+            int quotient = a / b;
+            
+            int remainder = a % b;
+
+
+            if (a == 17 && b == 4);
+            Console.WriteLine($"{a}/{b} is {b} remainder 1");
+
+            
+            
+            
+               
+           
+         
+             
+
+            
+        }
+    }
+}
